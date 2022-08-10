d1d13fe3edf7a4b6267c70c0f7653bb983d87689
lavitz <leojimlavi@gmail.com>
2022-08-10 15:16:05 -0500

    responsive - styled components

---
+++ src/utils/responsive.ts
@@ -0,0 +1,25 @@
+import { css } from "styled-components";
+
+export const mobile = (props: string) => {
+  return css`
+    @media only screen and (max-width: 426px) {
+      ${props}
+    }
+  `;
+};
+
+export const tablet = (props: string) => {
+  return css`
+    @media only screen and (max-width: 769px) {
+      ${props}
+    }
+  `;
+};
+
+export const desktop = (props: string) => {
+  return css`
+    @media only screen and (max-width: 1440px) {
+      ${props}
+    }
+  `;
+};
